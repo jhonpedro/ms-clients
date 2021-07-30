b51a9d16117f44dd2a73844846c47dd7bcdce063
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-30 10:53:59 -0300

    feat: create operations table

--- src/database/migrations/1627103001111-clients.ts
+++ src/database/migrations/1627103001111-clients.ts
@@ -1,12 +1,18 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+import {
+	MigrationInterface,
+	QueryRunner,
+	Table,
+	TableForeignKey,
+} from 'typeorm'
 
 export class clients1627103001111 implements MigrationInterface {
-	tableName = 'clients'
+	clientsTableName = 'clients'
+	operationsTableName = 'operations'
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.createTable(
 			new Table({
-				name: this.tableName,
+				name: this.clientsTableName,
 				columns: [
 					{
 						name: 'id',
@@ -24,6 +30,12 @@ export class clients1627103001111 implements MigrationInterface {
 					{
 						name: 'email',
 						type: 'varchar',
+						isUnique: true,
+						isNullable: false,
+					},
+					{
+						name: 'password',
+						type: 'varchar',
 						isNullable: false,
 					},
 					{
@@ -34,38 +46,104 @@ export class clients1627103001111 implements MigrationInterface {
 					{
 						name: 'cpf',
 						type: 'varchar',
+						isUnique: true,
 						isNullable: false,
 					},
 					{
 						name: 'income',
-						type: 'decimal',
+						type: 'real',
 						isNullable: false,
-						precision: 2,
 					},
 					{
 						name: 'balance',
-						type: 'decimal',
+						type: 'real',
 						isNullable: false,
 						default: 0,
-						precision: 2,
 					},
 					{
 						name: 'status',
 						type: 'enum',
 						enum: ['pending', 'approved', 'denied'],
-						default: 'pending',
+						enumName: 'status_enum',
+						default: `'pending'`,
 					},
 					{
 						name: 'status_updated_at',
 						type: 'timestamp',
-						default: new Date(),
+						default: 'NOW()',
+					},
+					{
+						name: 'forgot_password_token',
+						type: 'varchar',
+						isNullable: true,
+						isUnique: true,
 					},
 				],
 			})
 		)
+		await queryRunner.createTable(
+			new Table({
+				name: this.operationsTableName,
+				columns: [
+					{
+						name: 'id',
+						type: 'integer',
+						isGenerated: true,
+						generationStrategy: 'increment',
+						isPrimary: true,
+					},
+					{
+						name: 'client_id',
+						type: 'integer',
+						isNullable: false,
+					},
+					{
+						name: 'to_client',
+						type: 'integer',
+						isNullable: true,
+					},
+					{
+						name: 'cost',
+						type: 'real',
+						isNullable: false,
+					},
+					{
+						name: 'current_balance',
+						type: 'real',
+						isNullable: false,
+					},
+					{
+						name: 'previous_balance',
+						type: 'real',
+						isNullable: false,
+					},
+					{
+						name: 'created_at',
+						type: 'timestamp',
+						default: 'NOW()',
+					},
+				],
+			})
+		)
+
+		queryRunner.createForeignKeys(this.operationsTableName, [
+			new TableForeignKey({
+				name: this.operationsTableName,
+				columnNames: ['client_id'],
+				referencedColumnNames: ['id'],
+				referencedTableName: this.clientsTableName,
+			}),
+			new TableForeignKey({
+				name: this.operationsTableName,
+				columnNames: ['client_id'],
+				referencedColumnNames: ['id'],
+				referencedTableName: this.clientsTableName,
+			}),
+		])
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-		queryRunner.dropTable(this.tableName)
+		await queryRunner.dropTable(this.clientsTableName)
+		await queryRunner.dropTable(this.operationsTableName)
 	}
 }
