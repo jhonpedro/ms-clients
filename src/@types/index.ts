21ed267c0670cadcfc0cc4e09f8ec4d14c51b9a2
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-30 11:11:53 -0300

    style: add password in new client and JWTAuthToken

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,6 +1,7 @@
 export interface NewClient {
 	name: string
 	email: string
+	password: string
 	cpf: string
 	adress: string
 	income: number
@@ -10,4 +11,13 @@ export enum StatusEnum {
 	pending = 'pending',
 	approved = 'approved',
 	denied = 'denied',
+}
+
+/**
+ * iat and exp probably in unix timestamp
+ */
+export interface JWTAuthToken {
+	id: number
+	iat: number
+	exp: number
 }
