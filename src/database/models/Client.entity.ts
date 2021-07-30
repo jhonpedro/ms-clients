bf43a6c8cc13fd757a1f14d91eaf0870169deebd
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-30 11:03:52 -0300

    feat: add columns and search by cpf or email

--- src/database/models/Client.entity.ts
+++ src/database/models/Client.entity.ts
@@ -1,12 +1,17 @@
 import {
+	BeforeInsert,
+	BeforeUpdate,
 	Column,
 	Entity,
+	OneToMany,
 	PrimaryGeneratedColumn,
 	UpdateDateColumn,
 } from 'typeorm'
 import { StatusEnum } from '../../@types'
+import bcrypt from 'bcrypt'
+import Operation from './Operation.entity'
 
-@Entity()
+@Entity({ name: 'clients' })
 class Client {
 	@PrimaryGeneratedColumn()
 	id: number
@@ -17,6 +22,9 @@ class Client {
 	@Column()
 	email: string
 
+	@Column({ select: false })
+	password: string
+
 	@Column()
 	cpf: string
 
@@ -26,11 +34,31 @@ class Client {
 	@Column('decimal')
 	income: number
 
+	@Column('decimal')
+	balance: number
+
 	@Column('enum', { enum: ['pending', 'approved', 'denied'] })
 	status: StatusEnum
 
 	@UpdateDateColumn({ default: new Date() })
 	status_updated_at: Date
+
+	@Column()
+	forgot_password_token: string
+
+	@OneToMany(() => Operation, (operation) => operation.client_from)
+	operations: Operation[]
+
+	@OneToMany(() => Operation, (operation) => operation.client_to)
+	received_operations: Operation[]
+
+	@BeforeUpdate()
+	@BeforeInsert()
+	beforeInsertHook() {
+		if (!!this.password) {
+			this.password = bcrypt.hashSync(this.password, 6)
+		}
+	}
 }
 
 export default Client
--- src/database/repositories/Client.ts
+++ src/database/repositories/Client.ts
@@ -2,6 +2,14 @@ import { EntityRepository, Repository } from 'typeorm'
 import Client from '../models/Client.entity'
 
 @EntityRepository(Client)
-class ClientRepository extends Repository<Client> {}
+class ClientRepository extends Repository<Client> {
+	getByEmailOrCPFOrFail(emailOrCPF: string) {
+		if (emailOrCPF.includes('@')) {
+			return this.findOneOrFail({ where: { email: emailOrCPF } })
+		}
+
+		return this.findOneOrFail({ where: { cpf: emailOrCPF } })
+	}
+}
 
 export default ClientRepository
