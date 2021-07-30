62fb547c64c43cb770b3d76c9c243e630a649525
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-30 11:12:43 -0300

    feat: add routes and kafka consumers

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,41 @@
 import Express from 'express'
+import 'express-async-errors'
+
 import './database/connection'
+import runConsumers from './kafka/consumers'
+import errorMiddleware from './middleware/Error'
+import ClientRoutes from './routes/clients'
+import ForgotPasswordRoutes from './routes/forgotPasswords'
+import SessionRoutes from './routes/session'
+import OperationRoutes from './routes/operations'
 
 class App {
 	public app = Express()
 
 	constructor() {
 		this.middleware()
+		this.routes()
+		this.kafka()
+		this.errorMiddlewares()
 	}
 
 	middleware() {
 		this.app.use(Express.json())
+	}
+
+	routes() {
+		this.app.use('/clients', ClientRoutes)
+		this.app.use('/forgot-passwords', ForgotPasswordRoutes)
+		this.app.use('/session', SessionRoutes)
+		this.app.use('/operations', OperationRoutes)
+	}
+
+	errorMiddlewares() {
+		this.app.use(errorMiddleware)
+	}
+
+	kafka() {
+		runConsumers()
 	}
 }
 
